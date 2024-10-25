b87e031a1f206cc02b0d784b2e5e35db00890dc4
LucasOliveira332 <lucas.g.oliveira1@hotmail.com>
2024-10-25 20:52:59 -0300

    feat: initial structure

---
+++ src/test/java/dev/lucas/grana/GranaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucas.grana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
